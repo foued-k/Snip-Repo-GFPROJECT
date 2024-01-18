15118caef27bc4c18f77723b0db55cb8c9c98cc2
jelena2803 <jelenaiva2803@gmail.com>
2024-01-18 23:20:43 +0200

    Navbar components created for different pages

---
+++ client/src/components/NavbarDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavbarDashboard() {
+    return (
+<nav><h3 className="logo">SnipRepo</h3></nav>
+    )
+}
+
+export default NavbarDashboard;
---
+++ client/src/components/NavbarMain.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavbarMain() {
+    return (
+<nav><h3 className="logo-main">SnipRepo</h3></nav>
+    )
+}
+
+export default NavbarMain;
