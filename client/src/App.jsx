9604019b8e2df05b9f2d8c14164d2886864e50d1
jelena2803 <jelenaiva2803@gmail.com>
2024-01-19 04:54:45 +0200

    Components were rendered to be tested for the layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+// import { Route,Routes } from 'react-router-dom';
+// import NavbarMain from "./components/NavbarMain";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import NavbarDashboard from './components/NavbarDashboard';
+import AddSnippet from './components/AddSnippet';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div>
+      {/* <Routes>        
+      <Route path="/"  element={<Signup/>} />
+      <Route path="/login"  element={<Login />}/> */}
+      {/* <Route path="/dashboard"  element={<Dashboard/>}/> */}
+      {/* <Route path="/profile"  element={<Profile/>}/>
+      <Route path="/fitness"  element={<Fitness/>}/>
+      <Route path="/weight"  element={<Weight/>}/> */}
+      {/* </Routes> */}
+      <NavbarDashboard />
+      <Signup />
+      <Login />
+      <AddSnippet />
+
     </div>
   );
 }
