820abeb02514ae3d020d0570fe71d5ca3afb9e35
jelena2803 <jelenaiva2803@gmail.com>
2024-01-19 04:59:32 +0200

    NavbarMain component created

--- client/src/components/NavbarMain.jsx
+++ client/src/components/NavbarMain.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function NavbarMain() {
     return (
-<nav><h3 className="logo-main">SnipRepo</h3></nav>
+        <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          {/* <Navbar.Brand className="text-center nav-brand">SnipRepo</Navbar.Brand> */}
+          <h4>SnipRepo</h4>
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav> */}
+        </Container>
+      </Navbar>
+
+        
     )
 }
 
