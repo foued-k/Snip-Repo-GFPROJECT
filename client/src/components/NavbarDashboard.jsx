4ec48ea151538dc52a54983e80c99f1c97ad4c55
jelena2803 <jelenaiva2803@gmail.com>
2024-01-19 04:58:43 +0200

    NavbarDashboard component created

--- client/src/components/NavbarDashboard.jsx
+++ client/src/components/NavbarDashboard.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function NavbarDashboard() {
     return (
-<nav><h3 className="logo">SnipRepo</h3></nav>
+        <Navbar bg="light" data-bs-theme="light" className="justify-content-between">
+            <Container>
+                <Navbar.Brand className="text-center nav-brand">SnipRepo</Navbar.Brand>
+                {/* <h4>SnipRepo</h4> */}
+                {/* <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav> */}
+           
+                <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="dark">Search</Button>
+          </Form>
+
+
+          <Form className="d-flex">
+            <span className="me-2">Hello username</span>
+            <Button className="logout-btn">Logout</Button>
+          </Form>
+
+
+            </Container>
+        </Navbar>
+
     )
 }
+
 
 export default NavbarDashboard;
