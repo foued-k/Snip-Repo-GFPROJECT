62a050214dcaeeec69dc7753614114182bd6e311
jelena2803 <jelenaiva2803@gmail.com>
2024-01-20 04:49:36 +0200

    navbar component added

--- client/src/components/AddSnippet.jsx
+++ client/src/components/AddSnippet.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavbarSnippets from "./NavbarSnippets";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,61 +11,60 @@ import Form from 'react-bootstrap/Form';
 function AddSnippet() {
 
     return (
-        <Container className="signup-container">
-            <Row>
-                <Col className="signup-leftcol">
-                    <div className="snippetPreview">
+        <div>
+            <NavbarSnippets />
+            
+            <Container className="signup-container">
+                <Row>
+                    <Col className="signup-leftcol">
+                        <div className="snippetPreview">
 
-
-
-                    </div>
-
-                </Col>
-                <Col>
-
-                    <div className="signup-form">
-
-                        <FloatingLabel
-                            controlId="floatingInput"
-                            label="Snippet title"
-                            className="mb-3"
-                        >
-                            <Form.Control type="text" placeholder="title" onChange={(e) => console.log(e.target.value)} />
-                        </FloatingLabel>
-
-                        <FloatingLabel
-                            controlId="floatingInput"
-                            label="Snippet description"
-                            className="mb-3"
-                        >
-                            <Form.Control type="text" placeholder="description" onChange={(e) => console.log(e.target.value)} />
-                        </FloatingLabel>
-
-                        <Form.Select aria-label="Default select example">
-                            <option>Choose a snippet language</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </Form.Select>
-
-
-                        <div>
-                            <Form.Label className="add-code-title">Add your code here</Form.Label>
-                            <Form.Control as="textarea" placeholder="Enter your code" onChange={(e) => console.log(e.target.value)} />
                         </div>
 
-                        <div className="form-btn">
-                            <Button className="logout-btn">Add snippet</Button>
+                    </Col>
+                    <Col>
+
+                        <div className="signup-form">
+
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Snippet title"
+                                className="mb-3"
+                            >
+                                <Form.Control type="text" placeholder="title" onChange={(e) => console.log(e.target.value)} />
+                            </FloatingLabel>
+
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Snippet description"
+                                className="mb-3"
+                            >
+                                <Form.Control type="text" placeholder="description" onChange={(e) => console.log(e.target.value)} />
+                            </FloatingLabel>
+
+                            <Form.Select aria-label="Default select example">
+                                <option>Choose a snippet language</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+
+                            <div>
+                                <Form.Label className="add-code-title">Add your code here</Form.Label>
+                                <Form.Control as="textarea" placeholder="Enter your code" onChange={(e) => console.log(e.target.value)} />
+                            </div>
+
+                            <div className="form-btn">
+                                <Button className="logout-btn">Add snippet</Button>
+                            </div>
+
                         </div>
 
-                    </div>
-
-                </Col>
-            </Row>
-        </Container>
-
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
-
 }
 
 export default AddSnippet;
