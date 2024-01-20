24d7167889753033d159977c3bc009c78fb99e82
jelena2803 <jelenaiva2803@gmail.com>
2024-01-20 04:56:49 +0200

    navbar component completed

--- client/src/components/NavbarSnippets.jsx
+++ client/src/components/NavbarSnippets.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 
 function NavbarSnippets() {
     return (
-<nav><h3 className="logo">SnipRepo</h3></nav>
+        <Navbar bg="light" data-bs-theme="light" className="justify-content-between">
+            <Container>
+                <Navbar.Brand className="text-center nav-brand"><Link to={'/dashboard'} className="links">Dashboard</Link></Navbar.Brand>
+                <h4>SnipRepo</h4>
+                <Button className="logout-btn">Logout</Button>
+            </Container>
+        </Navbar>
     )
 }
 
