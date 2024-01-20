f99e8f8a44b278bd824fefeacaf9c40cb30cdc7b
jelena2803 <jelenaiva2803@gmail.com>
2024-01-20 04:57:36 +0200

    navbar added

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
+import NavbarMain from "./NavbarMain";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-// import {Link} from "react-router-dom";
 
 function Signup() {
     // const navigate = useNavigate();
@@ -17,72 +18,70 @@ function Signup() {
     // const [error, setError] = useState("");
 
     function signup() {
-        axios.post("http://localhost:3020/signup", {username : username, password : password,})
-        .then(({data}) => {
-            console.log(data);
-            setUsername("");
-            setPassword("");
-            // if (data.msg === "user created successfully") {
-            //     //redirect the user to the login page
-            //     // navigate("/login");
-            // } else {
-            //     setError(data.msg);
-            // }
-        })
+        axios.post("http://localhost:3020/signup", { username: username, password: password, })
+            .then(({ data }) => {
+                console.log(data);
+                setUsername("");
+                setPassword("");
+                // if (data.msg === "user created successfully") {
+                //     //redirect the user to the login page
+                //     // navigate("/login");
+                // } else {
+                //     setError(data.msg);
+                // }
+            })
     }
 
-
     return (
-        <Container className="signup-container">
-            <Row>
-                <Col className="signup-leftcol">
-                    <div>
-                        <p className="signup-bodytext">
 
-                            SnipRepo is a powerful and user-friendly web application
-                            designed to streamline the management and organization of code snippets for developers.
-                            Whether you're a seasoned programmer or a coding enthusiast,
-                            SnipRepo provides an intuitive platform to store, categorize, search, and share your code snippets effortlessly.
+        <div>
+            <NavbarMain />
 
-                        </p>
-                        <img src={require('../images/Img1.png')} className="img" alt={""} />
-                    </div>
-
-                </Col>
-                <Col>
-
-                    <div className="signup-form">
-                        <h3>Create an account</h3>
-                        <h5>Let's start organising</h5>
-
-                        <FloatingLabel
-                            controlId="floatingInput"
-                            label="Username"
-                            className="mb-3"
-                        >
-                            <Form.Control type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)}/>
-                        </FloatingLabel>
-
-                        <FloatingLabel controlId="floatingPassword" label="Password">
-                            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                        </FloatingLabel>
+            <Container className="signup-container">
+                <Row>
+                    <Col className="signup-leftcol">
                         <div>
-
+                            <p className="signup-bodytext">
+                                SnipRepo is a powerful and user-friendly web application designed to streamline the management and organization of code snippets for developers.
+                                Whether you're a seasoned programmer or a coding enthusiast,
+                                SnipRepo provides an intuitive platform to store, categorize, search, and share your code snippets effortlessly.
+                            </p>
+                            <img src={require('../images/Img1.png')} className="img" alt={""} />
                         </div>
 
-                        <div className="form-btn">
-                            <Button variant="dark" onClick={()=>signup()}>Sign up</Button>
+                    </Col>
+                    <Col>
+
+                        <div className="signup-form">
+                            <h3>Create an account</h3>
+                            <h5>Let's start organising</h5>
+
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Username"
+                                className="mb-3"
+                            >
+                                <Form.Control type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} />
+                            </FloatingLabel>
+
+                            <FloatingLabel controlId="floatingPassword" label="Password">
+                                <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                            </FloatingLabel>
+                            <div>
+
+                            </div>
+
+                            <div className="form-btn">
+                                <Button variant="dark" onClick={() => signup()}>Sign up</Button>
+                            </div>
+
+                            <p>Already have an account? <Link to={`/login`} className="homepage-account-link">Log in</Link></p>
                         </div>
-
-                        <p>Already have an account? Log in</p>
-                    </div>
-                    {/* <p>Already have an account? <Link to={`/login`}>Log in</Link></p> */}
-                </Col>
-            </Row>
-        </Container>
-
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
-
 }
 
 export default Signup;
