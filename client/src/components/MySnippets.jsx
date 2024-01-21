f4031c4703f96ca44461cc3ff3e3d04cb2d0147c
jelena2803 <jelenaiva2803@gmail.com>
2024-01-21 04:14:55 +0200

    component completed and tested for rendering of all the snippets and deleting a single snippet on click

--- client/src/components/MySnippets.jsx
+++ client/src/components/MySnippets.jsx
@@ -1,42 +1,46 @@
-import React, { useEffect , useState } from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import NavbarSnippets from "./NavbarSnippets";
 import Card from 'react-bootstrap/Card';
+import { useCookies } from "react-cookie";
 
 function MySnippets() {
     const [snippets, setSnippets] = useState([]);
+    const [cookies] = useCookies(["token"]);
+    const token = cookies.token;
+
+    useEffect(() => {
+        if (token) {
+            getSnippets()
+        } else {
+            console.error("Token is missing");
+            return;
+        }
+    }, []);
 
     // get all the snippets from the database and render the result
-    // function getSnippets() {
-    //     try {
-    //         console.log("weight requested front")
-    //         axios.get("http://localhost:3020/snippets")
-    //             .then(({ data }) => {
-    //                 setSnippets(data);
-    //                 console.log(snippets);
-    //             })
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-
-    // if a snippet gets deleted and snippets collection changes the page renders again
-    // useEffect(() => {getSnippets}, [snippets]);
+    function getSnippets() {
+        try {
+            axios.get("http://localhost:3020/snips", {
+                withCredentials: true
+            })
+                .then(({ data }) => {
+                    setSnippets(data.snips);
+                })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     //  delete a single snippet and return the updated collection
-    // const deleteSnippet = async (_id) => {
-    //    const res = await axios.delete(`http://localhost:3020/snips/${_id}`);
-
-    //    const updatedSnippets = [...snippets].filter((e) => {
-    //     return e._id != _id;
-    //    });
-
-    //    setSnippets(updatedSnippets);
-
-    // }
+    const deleteSnippet = async (id) => {
+        const res = await axios.delete(`http://localhost:3020/snips/${id}`, {
+            withCredentials: true
+        });
+        getSnippets();
+    }
 
     return (
-
         <div>
             <NavbarSnippets />
 
@@ -46,58 +50,26 @@ function MySnippets() {
 
                 <div className="d-flex gap-5">
 
-                    <Card>
-                        <Card.Header>
-                       <img src={require('../images/trash.png')} className="img justify-content-end" alt={""} />
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Title>Snippet Title</Card.Title>
-                            <Card.Text>
-                                Language
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-
-                    <Card>
-                        <Card.Header>
-                            <img src={require('../images/trash.png')} className="img justify-content-end" alt={""} /> </Card.Header>
-                        <Card.Body>
-                            <Card.Title>Snippet Title</Card.Title>
-                            <Card.Text>
-                                Language
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-
-                    <Card>
-                        <Card.Header>
-                            <img src={require('../images/trash.png')} className="img justify-content-end" alt={""} />
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Title>Snippet Title</Card.Title>
-                            <Card.Text>
-                                Language
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-
-                    {/* <div>
-                {snippets.map((e) => {
-                    return (
-                        <Card>
-                <Card.Header>
-                   <img src={require('../images/trash.png')} className="img justify-content-end" alt={""} />
-                    </Card.Header>
-                    <Card.Body>
-                        <Card.Title>{e.title}</Card.Title>
-                        <Card.Text>
-                            {e.language}
-                        </Card.Text>         
-                    </Card.Body>
-                </Card>
+                    {snippets.map((e) => {
+                        return (
+                            <Card key={e._id}>
+                                <Card.Header>
+                                    <img src={require('../images/trash.png')} className="img justify-content-end" alt={""} onClick={() => deleteSnippet(e._id)} />
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>{e.title}</Card.Title>
+                                    <Card.Text>
+                                        {e.language}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        )
+                    }
                     )}
-                )}
-            </div> */}
+
+                    <div>
+
+                    </div>
 
                 </div>
             </div>
