e3cfe7f3c68adc1fb50199cc415435e49c0da8eb
Madalena Bastakou <madalena.bastakou@gmail.com>
2024-01-21 16:27:03 +0200

    Add snippet functionality / fetch username to display on navbar

--- client/src/components/NavbarMain.jsx
+++ client/src/components/NavbarMain.jsx
@@ -1,9 +1,13 @@
+
 import React from "react";
 import Container from 'react-bootstrap/Container';
 // import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
 function NavbarMain() {
+ 
+
     return (
         <Navbar bg="light" data-bs-theme="light" id="main-nav-title">
         <Container>
